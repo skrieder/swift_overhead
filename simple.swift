93bb9757d5d87cc8f545c8f960d68e1ea8718c95
bgrim <ben.grimmer@ossm.edu>
2012-06-19 09:01:18 -0500

    added fastest swift program I could make. It doesn't use sleep because calling external operations requires you to make an output file for it

---
+++ simple.swift
@@ -0,0 +1,10 @@
+//This is the simplest swift program I could make.
+//It doesn't use sleep because to call the external operation
+// of sleep, I would need to make a file for it to output to.
+//Making a file will cause slower execution and give us a less
+//  accurate overhead.
+
+//In the few runs I did with this using the time operation. I
+//  found it averaged an overhead of 1.8 seconds
+
+int i = @toint("1");
